f49560c5e8ab06ad240b3938811326b697819173
onoway68 <onalaolanrewaju12@gmail.com>
2025-05-31 08:39:32 -0500

    davidarrowtech.com-platform
    I am building a platform as a service for Davjd Arrow Technologies(DA Teck)

--- app/routes/da_education.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-bp = Blueprint('da_education', __name__, url_prefix='/education')
-
-@bp.route('/')
-def index():
-    return render_template('da_education.html')
--- app/routes/da_health.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-bp = Blueprint('da_health', __name__, url_prefix='/health')
-
-@bp.route('/')
-def index():
-    return render_template('da_health.html')
